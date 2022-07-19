15951e724ed3525d8edd9fdd272dea4f1ac29cc2
BraveSejin <ihavebravebiz@gmail.com>
2022-07-19 12:30:37 +0900

    suspend function

--- kotlin_examples/src/main/kotlin/Main.kt
+++ kotlin_examples/src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class User(val name: String, val contribution: Int)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main(args: Array<String>) {
+    val testList = listOf(listOf("A", "B"), listOf("C", "D"))
+
+    testList.flatMap { it -> it }
+        .also { println(it) }
+    testList.map { it -> it }
+        .also { println(it) }
+
+    val userList =
+        mutableListOf<User>().apply {
+            add(User("Sejin", 1))
+            add(User("Sejin", 2))
+            add(User("Sejin", 1))
+            add(User("Hyunsu", 1))
+            add(User("Hyunsu", 2))
+            add(User("root", 1))
+        }
+
+    val test = userList.groupBy { it.name } // map 형태로 나옴 (유저 : 유저 리스트(group )
+            .map { (name, group) ->
+                User(name, group.sumBy { //
+                    it.contribution
+                })
+            }
+    println(test)
+
 }
+
+//fun MutableList<User>.aggregate(): MutableList<User>
+//    = groupBy { it.name }
---
+++ kotlin_examples/src/main/kotlin/coroutineEx.kt
@@ -0,0 +1,12 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val deffered = async {
+
+    }
+}
+
+suspend fun loadData(): Int:{
+    log("loading")
+}
